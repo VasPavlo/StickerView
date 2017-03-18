870e642bd6c6ea77ec95bf9c0b7518db9f8c19ee
VasPavlo <grubd@mail.ru>
2017-03-18 10:55:28 +0200

    Init

---
+++ StickerViewExample/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+
+
+//////https://github.com/kencheung4/android-StickerView
+//////https://github.com/niravkalola/Android-StickerView
+
+
+namespace StickerViewExample
+{
+	[Activity(Label = "StickerViewExample", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			FrameLayout canvas = (FrameLayout)FindViewById(Resource.Id.canvasView);
+
+			StickerImageView iv_sticker = new StickerImageView(this);
+			iv_sticker.SetImageDrawable(Resources.GetDrawable(Resource.Drawable.c10));
+			canvas.AddView(iv_sticker);
+		}
+	}
+}
+
---
+++ StickerViewExample/StickerView/StickerImageView.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Android.Content;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace StickerViewExample
+{
+	public class StickerImageView : StickerView
+	{
+		private String owner_id;
+		private ImageView iv_main;
+		public StickerImageView(Context context) : base(context)
+		{
+
+		}
+
+		public StickerImageView(Context context, IAttributeSet attrs) : base(context, attrs)
+		{
+
+		}
+
+		public StickerImageView(Context context, IAttributeSet attrs, int defStyle) : base(context, attrs, defStyle)
+		{
+
+		}
+
+
+		public void SetOwnerId(String owner_id)
+		{
+			this.owner_id = owner_id;
+		}
+
+		public String GetOwnerId()
+		{
+			return this.owner_id;
+		}
+
+		public void SetImageResource(int res_id)
+		{
+			this.iv_main.SetImageResource(res_id);
+		}
+
+		public void SetImageDrawable(Drawable drawable) { iv_main.SetImageDrawable(drawable); }
+
+		public Bitmap GetImageBitmap() { return ((BitmapDrawable)this.iv_main.Drawable).Bitmap; }
+
+		protected override View GetMainView()
+		{
+			if (this.iv_main == null)
+			{
+				this.iv_main = new ImageView(Context);
+				this.iv_main.SetScaleType(ImageView.ScaleType.FitXy);
+			}
+			return iv_main;
+		}
+	}
+}
